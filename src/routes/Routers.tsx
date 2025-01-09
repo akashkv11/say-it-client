ef899a669a271eb45489076cabbadecd11727500
akashk11 <akashvinod777@gmail.com>
2025-01-09 21:48:05 +0530

    Added home page component

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -4,11 +4,13 @@ import Loader from "../components/Loader";
 
 const Routers = () => {
   const Login = lazy(() => import("../pages/Login"));
+  const HomePage = lazy(() => import("../pages/HomePage"));
 
   return (
     <BrowserRouter>
       <Suspense fallback={<Loader />}>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
