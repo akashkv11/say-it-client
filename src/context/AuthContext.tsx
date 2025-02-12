c810c68b00d87ac3c679982ae4c6d9f63e5c97fa
akashk11 <akashvinod777@gmail.com>
2025-02-12 21:28:53 +0530

    Authentication with route protection

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,68 @@
+// src/context/AuthContext.tsx
+import React, { createContext, useContext, useState } from "react";
+import api from "../utils/axios-instance";
+// src/types/auth.ts
+export interface User {
+  id: string;
+  username: string;
+}
+type FormValues = {
+  email: string;
+  password: string;
+  remember: boolean;
+};
+export interface AuthContextType {
+  user: User | null;
+  login: (values: FormValues) => Promise<boolean>;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+0;
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  const login = async (values: FormValues) => {
+    try {
+      const response = await api.post("/auth/signin", values);
+
+      const data = response?.data?.data;
+      if (response?.data?.success) {
+        const user: User = { id: data.id, username: data.username };
+        localStorage.setItem("token", response.data.data.access_token);
+        localStorage.setItem("user", JSON.stringify(user));
+      }
+
+      setUser(user);
+      return true
+    } catch (error) {
+      console.error("Login error:", error);
+      return false
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Loader from "../components/Loader";
+import PrivateRoute from "../privateRoute/PrivateRoute";
 
 const Routers = () => {
   const Login = lazy(() => import("../pages/Login/Login"));
@@ -10,9 +11,11 @@ const Routers = () => {
     <BrowserRouter>
       <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
+          </Route>
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
       </Suspense>
