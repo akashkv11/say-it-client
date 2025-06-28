6eeea1b371163f605240951f778bf09f9587fcb3
akashk11 <akashvinod777@gmail.com>
2025-06-28 22:20:49 +0530

    Add routing for SignUp page and update Dashboard route

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -6,15 +6,18 @@ import PrivateRoute from "../privateRoute/PrivateRoute";
 const Routers = () => {
   const Login = lazy(() => import("../pages/Login/Login"));
   const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+  const Dashboard = lazy(() => import("../pages/Dashboard/Dashboard"));
+  const SignUp = lazy(() => import("../pages/SignUp/SignUp"));
 
   return (
     <BrowserRouter>
       <Suspense fallback={<Loader />}>
         <Routes>
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route element={<PrivateRoute />}>
             <Route path="/" element={<HomePage />} />
-            <Route path="/home" element={<HomePage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Route>
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
