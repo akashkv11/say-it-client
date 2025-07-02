80a22ddba810fc7cec4b30740833417b6bf1263a
akashk11 <akashvinod777@gmail.com>
2025-07-02 23:10:30 +0530

    Refactor user.service.ts to correct import paths and add getChatWithSelectedUser function for fetching chat messages with a selected user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,9 @@
 import { AxiosResponse } from "axios";
 import { User } from "../context/AuthContext";
 import api from "../lib/axios";
+import { ResponseType } from "../types/response.type";
 import tryCatch from "../utils/try-catch";
-import { ResponseType } from "../types/response-type";
+import { ChatMessageType } from "../types/message.types";
 
 export const getAllUsers = async () => {
   const { data: result, error } = await tryCatch<
@@ -10,6 +11,19 @@ export const getAllUsers = async () => {
   >(api.get("/users"));
   if (error) {
     console.error("Error fetching users:", error);
+    return [];
+  }
+  return result.data.data;
+};
+
+export const getChatWithSelectedUser = async (
+  recipientId: string
+): Promise<ChatMessageType[]> => {
+  const { data: result, error } = await tryCatch<
+    AxiosResponse<ResponseType<ChatMessageType[]>>
+  >(api.get(`/users/messages/${recipientId}`));
+  if (error) {
+    console.error("Error fetching chat messages:", error);
     return [];
   }
   return result.data.data;
